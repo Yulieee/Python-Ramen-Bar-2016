84f73526c78c7fdd657d965d7fd681b8daa313fb
Sara Ng <sara.blalock.ng@gmail.com>
2016-12-05 15:46:51 -0700

    interface better
    changing the size of the picture and new font/background color.

--- order_food.py
+++ order_food.py
@@ -1,29 +1,38 @@
 from tkinter import *
 from dialogue_manager import parse_sentence, respond, order
+import PIL
 
 class App(Frame):
-    
-    def __init__(self, master=None):
+   
+    def __init__(self, master=None, *pargs):
         
-        Frame.__init__(self, master)
+        Frame.__init__(self, master, *pargs)
         self.master.title("Welcome to the Python Ramen Bar!")
         self.master.minsize(1000, 500)
         self.grid(row = 0)
-        self.columnconfigure(1, weight = 1)
-        
-        self.response = Label(text = "Can I help who's next?")
+        self.columnconfigure(1, weight = 1)               
+        self.response = Label(text = "Can I help who's next?", bg="bisque1")
         self.response.grid(row = 0, column = 0, columnspan = 2, sticky = W+E)
-        
-        self.sentence_entry = Entry()
+
+        self.master.configure(bg = 'bisque1')
+
+        # for the entry bar
+        self.sentence_entry = Entry(bg="DarkOrange2", justify='center')
         self.sentence_entry.grid(row = 1, column = 0, columnspan = 2, sticky = W+E)
-        
+
+        # for the menu
+        #self.menu = PIL.Image.open('menu.gif')
+
         self.menu_image = PhotoImage(file = 'menu.gif')
-        self.menu_image_label = Label(image = self.menu_image)
+        self.menu_copy = self.menu_image.copy().subsample(2)
+        
+        self.menu_image_label = Label(master, image = self.menu_copy)
         self.menu_image_label.grid(row = 2, column = 1)
-        
-        self.order_label = Label(text = 'Nothing ordered yet.')
+
+        # for the list of order
+        self.order_label = Label(text = 'Nothing ordered yet.', bg="bisque1")
         self.order_label.grid(row = 2, column = 0, sticky = W+E+N+S)
-        
+            
         # This variable holds the sentence that the user typed in.
         self.sentence = StringVar()
         # It's bound to the text displayed in the entry field in the GUI.
@@ -31,11 +40,6 @@ class App(Frame):
         
         # When the user hits return, parse the sentence and print the results.
         self.sentence_entry.bind('<Key-Return>', self.manage_sentence)
-        
-#        print()
-#        log_file = open('uuscil_log.txt', 'a')
-#        log_file.write('------------------------\n')
-#        log_file.close()
 
     def manage_sentence(self, event):
         user_sentence = self.sentence.get()
@@ -48,6 +52,8 @@ class App(Frame):
         print()
 
 root = Tk()
+cuter = font.Font(family="Kristen ITC",size=12,weight="normal")
+root.option_add("*Font", cuter)
 root.columnconfigure(index = 0, weight = 1)
 app = App(master = root)
 app.mainloop()
